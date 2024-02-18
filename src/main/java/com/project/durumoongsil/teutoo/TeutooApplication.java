3a3f4c258f384e0f681d77b74885895448653442
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-02-18 17:40:38 +0900

    first commit

---
+++ src/main/java/com/project/durumoongsil/teutoo/TeutooApplication.java
@@ -0,0 +1,13 @@
+package com.project.durumoongsil.teutoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeutooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeutooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/durumoongsil/teutoo/TeutooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.durumoongsil.teutoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeutooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
