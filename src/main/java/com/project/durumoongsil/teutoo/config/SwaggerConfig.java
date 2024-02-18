5914a05f045ed7c53af39ca9344cca62d8ed44b7
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-02-18 20:36:41 +0900

    swagger springdoc-ui 추가
    swagger 관련 프로퍼티 및 설정 추가

---
+++ src/main/java/com/project/durumoongsil/teutoo/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.project.durumoongsil.teutoo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String API_NAME = "TEUTOO API";
+    private static final String API_VERSION = "1.0.0";
+    private static final String API_DESCRIPTION = "description";
+
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION);
+
+        final String securitySchemeName = "jwt Auth";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(securitySchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 임의로 JWT 토큰 지정
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+
+}
