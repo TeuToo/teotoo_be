a5442b13b11109dccb4b77bb063d9a485ee219a0
juhwan <asdcz@naver.com>
2024-02-22 13:17:38 +0900

    회원 업데이트 로직

--- src/main/java/com/project/durumoongsil/teutoo/member/service/MemberService.java
+++ src/main/java/com/project/durumoongsil/teutoo/member/service/MemberService.java
@@ -1,13 +1,17 @@
 package com.project.durumoongsil.teutoo.member.service;
 
+import com.project.durumoongsil.teutoo.exception.NotFoundUserException;
 import com.project.durumoongsil.teutoo.member.domain.Member;
 import com.project.durumoongsil.teutoo.member.domain.Role;
 import com.project.durumoongsil.teutoo.member.dto.MemberJoinDto;
+import com.project.durumoongsil.teutoo.member.dto.MemberUpdateDto;
 import com.project.durumoongsil.teutoo.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static com.project.durumoongsil.teutoo.member.domain.Role.*;
 
@@ -28,6 +32,13 @@ public class MemberService {
         member.setRole(role);
 
         memberRepository.save(member);
+    }
+
+    public void updateInfo(Long id, MemberUpdateDto memberUpdateDto) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new NotFoundUserException("사용자를 찾을 수 없습니다."));
+
+        member.updateInfo(memberUpdateDto);
     }
 
     private Role getRole(MemberJoinDto memberJoinDto) {
