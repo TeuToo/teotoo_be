afdad2a44bd972859f8a50939ada42ebe4170328
juhwan <asdcz@naver.com>
2024-02-24 01:30:40 +0900

    데이터를 한번감싸는 객체 추가

---
+++ src/main/java/com/project/durumoongsil/teutoo/common/RestResult.java
@@ -0,0 +1,11 @@
+package com.project.durumoongsil.teutoo.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RestResult {
+
+    Object data;
+}
