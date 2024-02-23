b76a8ecee8e898fa8f2aacfd152cf397d846e731
juhwan <asdcz@naver.com>
2024-02-24 00:51:10 +0900

    Swagger 어노테이션 적용

--- src/main/java/com/project/durumoongsil/teutoo/login/controller/LoginController.java
+++ src/main/java/com/project/durumoongsil/teutoo/login/controller/LoginController.java
@@ -4,8 +4,12 @@ import com.project.durumoongsil.teutoo.login.dto.LoginDto;
 import com.project.durumoongsil.teutoo.login.dto.TokenDto;
 import com.project.durumoongsil.teutoo.security.jwt.JwtAuthenticationFilter;
 import com.project.durumoongsil.teutoo.security.jwt.TokenProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +20,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,9 +29,15 @@ public class LoginController {
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-
+    @Operation(summary = "로그인", description = "이메일과 비밀번호를 사용하여 로그인합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 토큰 발급"),
+            @ApiResponse(responseCode = "401", description = "로그인 실패"),
+            @ApiResponse(responseCode = "403", description = "권한이 없는 오류")
+    })
     @PostMapping("/login")
-    public ResponseEntity<TokenDto> authorize(@Validated @RequestBody LoginDto loginDto) {
+    public ResponseEntity<TokenDto> authorize(
+            @ParameterObject @Validated @RequestBody LoginDto loginDto) {
 
         log.info("loginDto ={}", loginDto);
 
