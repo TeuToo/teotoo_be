7bd5aea160d6be5da67a5113e794425302d5babe
juhwan <asdcz@naver.com>
2024-02-24 00:08:20 +0900

    Longin 시 토큰발급 로직

---
+++ src/main/java/com/project/durumoongsil/teutoo/login/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.project.durumoongsil.teutoo.login.controller;
+
+import com.project.durumoongsil.teutoo.login.dto.LoginDto;
+import com.project.durumoongsil.teutoo.login.dto.TokenDto;
+import com.project.durumoongsil.teutoo.security.jwt.JwtAuthenticationFilter;
+import com.project.durumoongsil.teutoo.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Slf4j
+public class LoginController {
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> authorize(@Validated @RequestBody LoginDto loginDto) {
+
+        log.info("loginDto ={}", loginDto);
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtAuthenticationFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
+}
