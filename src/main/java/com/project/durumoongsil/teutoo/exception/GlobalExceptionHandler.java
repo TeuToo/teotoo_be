89e66ffc5bc4d47216e673dfc26210c4e3492af6
juhwan <asdcz@naver.com>
2024-02-24 01:30:14 +0900

    로그인하지 않은 유저는 예외 추가

--- src/main/java/com/project/durumoongsil/teutoo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/durumoongsil/teutoo/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.project.durumoongsil.teutoo.exception;
 
+import com.project.durumoongsil.teutoo.common.RestError;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -24,5 +28,11 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UserUnauthorizedException.class)
+    public RestError handlingAuthorizationException(UserUnauthorizedException ex) {
+        return new RestError(HttpStatus.UNAUTHORIZED.toString(), ex.getMessage());
     }
 }
