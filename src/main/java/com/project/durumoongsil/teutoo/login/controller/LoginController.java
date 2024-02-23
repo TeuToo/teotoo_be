4ad5d9b7e8a8f4ca6fde02f6c4bb815d9ad8d8ba
juhwan <asdcz@naver.com>
2024-02-24 00:34:03 +0900

    RequestMapping 삭제

--- src/main/java/com/project/durumoongsil/teutoo/login/controller/LoginController.java
+++ src/main/java/com/project/durumoongsil/teutoo/login/controller/LoginController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
