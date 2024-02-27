05db972c3193ee4dc76215baa469327b10984b70
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-02-27 21:41:53 +0900

    트레이너 목록 조회 레포지터리 메소드 구현 - TrainerInfoCustomRepositoryImpl 추가: findBySearchCondition 메소드 추가 및 동적쿼리 구현

--- src/main/java/com/project/durumoongsil/teutoo/trainer/repository/TrainerInfoCustomRepository.java
+++ src/main/java/com/project/durumoongsil/teutoo/trainer/repository/TrainerInfoCustomRepository.java
@@ -1,10 +1,15 @@
 package com.project.durumoongsil.teutoo.trainer.repository;
 
 import com.project.durumoongsil.teutoo.member.domain.Member;
+import com.project.durumoongsil.teutoo.trainer.domain.TrainerInfo;
+import com.project.durumoongsil.teutoo.trainer.dto.TrainerListReqDto;
+import org.springframework.data.domain.Page;
 
 import java.util.Optional;
 
 public interface TrainerInfoCustomRepository {
 
     Optional<Member> findMemberByIdWithTrainerInfo(Long memberId);
+
+    Page<TrainerInfo> findBySearchCondition(TrainerListReqDto trainerListReqDto);
 }
