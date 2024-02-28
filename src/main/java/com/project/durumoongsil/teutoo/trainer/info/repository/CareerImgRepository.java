9b4d56494d46ce9eea733bc2e8d17ee1cd227b90
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-02-28 18:25:37 +0900

    CareerImgRepository 조회 방식 변경

--- src/main/java/com/project/durumoongsil/teutoo/trainer/info/repository/CareerImgRepository.java
+++ src/main/java/com/project/durumoongsil/teutoo/trainer/info/repository/CareerImgRepository.java
@@ -2,8 +2,15 @@ package com.project.durumoongsil.teutoo.trainer.info.repository;
 
 import com.project.durumoongsil.teutoo.trainer.info.domain.CareerImg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CareerImgRepository extends JpaRepository<CareerImg, Long>, CareerImgCustomRepository{
+public interface CareerImgRepository extends JpaRepository<CareerImg, Long> {
+
+    @Query("select ci from CareerImg ci join fetch ci.file where ci.trainerInfo.id = :trainerId")
+    List<CareerImg> findByTrainerIdWithFile(@Param("trainerId") Long trainerId);
 }
