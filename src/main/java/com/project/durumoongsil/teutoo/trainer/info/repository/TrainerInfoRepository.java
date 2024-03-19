0c8c64b53c53d2f2b58c0867bdd25707d4d67353
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-03-19 23:49:07 +0900

    TrainerInfoCustomRepository 디렉터리 변경

--- src/main/java/com/project/durumoongsil/teutoo/trainer/info/repository/TrainerInfoRepository.java
+++ src/main/java/com/project/durumoongsil/teutoo/trainer/info/repository/TrainerInfoRepository.java
@@ -1,6 +1,7 @@
 package com.project.durumoongsil.teutoo.trainer.info.repository;
 
 import com.project.durumoongsil.teutoo.trainer.info.domain.TrainerInfo;
+import com.project.durumoongsil.teutoo.trainer.info.repository.custom.TrainerInfoCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
