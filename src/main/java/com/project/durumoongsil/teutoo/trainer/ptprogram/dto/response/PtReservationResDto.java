b8a25625eabc84d6401b69b7fb77819a1bc49c1f
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-03-22 01:58:40 +0900

    PT 프로그램 예약 DTO 에 swagger 어노테이션 명시

--- src/main/java/com/project/durumoongsil/teutoo/trainer/ptprogram/dto/response/PtReservationResDto.java
+++ src/main/java/com/project/durumoongsil/teutoo/trainer/ptprogram/dto/response/PtReservationResDto.java
@@ -1,9 +1,13 @@
 package com.project.durumoongsil.teutoo.trainer.ptprogram.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "PT 프로그램 예약 Dto")
 @Getter
 public class PtReservationResDto {
+
+    @Schema(description = "PT 프로그램 예약을 한 이후 예약된 id")
     private Long reservationId;
 
     public PtReservationResDto(Long reservationId) {
