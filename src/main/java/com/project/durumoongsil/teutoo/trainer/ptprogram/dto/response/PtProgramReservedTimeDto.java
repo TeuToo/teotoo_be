afc6385fe860f5273617ae8dff4ffd5e8cf6ba6a
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-03-27 00:23:28 +0900

    Dto swagger 어노테이션 추가 및 Pt 트레이너 스케쥴 DTO 추가

--- src/main/java/com/project/durumoongsil/teutoo/trainer/ptprogram/dto/response/PtProgramReservedTimeDto.java
+++ src/main/java/com/project/durumoongsil/teutoo/trainer/ptprogram/dto/response/PtProgramReservedTimeDto.java
@@ -1,14 +1,18 @@
 package com.project.durumoongsil.teutoo.trainer.ptprogram.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "PT 프로그램 예약 시간 DTO")
 @Getter
 @Setter
 public class PtProgramReservedTimeDto {
+    @Schema(description = "예약 시작 시간")
     private LocalDateTime startDateTime;
+    @Schema(description = "예약 종료 시간")
     private LocalDateTime endDateTime;
 
     public PtProgramReservedTimeDto(LocalDateTime startDateTime, LocalDateTime endDateTime) {
