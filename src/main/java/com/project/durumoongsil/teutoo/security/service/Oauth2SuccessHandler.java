7da489c4fc34898a7359b28ef8c3544e01dae9dc
juhwan <asdcz@naver.com>
2024-04-01 21:29:55 +0900

    쿠키에 jwt 을 담아서 전송

--- src/main/java/com/project/durumoongsil/teutoo/security/service/Oauth2SuccessHandler.java
+++ src/main/java/com/project/durumoongsil/teutoo/security/service/Oauth2SuccessHandler.java
@@ -3,11 +3,11 @@ package com.project.durumoongsil.teutoo.security.service;
 import com.project.durumoongsil.teutoo.security.oauth2.CustomOauth2User;
 import com.project.durumoongsil.teutoo.security.jwt.TokenProvider;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -28,12 +28,20 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
             CustomOauth2User customUser = (CustomOauth2User) authToken.getPrincipal();
             String email = customUser.getEmail();
 
-            // 카카오에 저장된 이메일을 사용하여 토큰 생성
-            String token = tokenProvider.createToken(authToken, email);
+            String jwt = tokenProvider.createToken(authToken, email); // 토큰 생성
 
-            response.setContentType("application/json;charset=UTF-8");
-            response.setStatus(HttpStatus.OK.value());
-            response.getWriter().write("{\"access_token\": \"" + token + "\"}");
+            // 쿠키 생성
+            Cookie jwtCookie = new Cookie("token", jwt);
+            jwtCookie.setHttpOnly(false); // JavaScript를 통한 접근 방지 : true , 접근허용 : false
+            jwtCookie.setPath("/"); // 전체 도메인에 대해 쿠키 접근 허용
+            jwtCookie.setSecure(false); // 쿠키 보안을 위해 HTTPS 환경 : true HTTP : false
+            jwtCookie.setMaxAge(60 * 60);
+
+            // 쿠키를 응답에 추가
+            response.addCookie(jwtCookie);
+        } else {
+            // 적절한 오류 처리
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증 정보가 없습니다.");
         }
     }
 }
