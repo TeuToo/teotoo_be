e5d818e98a328e7b446b6aeebaf2c0d74eeb0037
juhwan <asdcz@naver.com>
2024-04-03 19:28:34 +0900

    유저 정보 수정 테스트 코드 수정

--- src/main/java/com/project/durumoongsil/teutoo/member/dto/MemberUpdateDto.java
+++ src/main/java/com/project/durumoongsil/teutoo/member/dto/MemberUpdateDto.java
@@ -3,12 +3,15 @@ package com.project.durumoongsil.teutoo.member.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Schema(description = "회원 수정 Dto")
 public class MemberUpdateDto {
