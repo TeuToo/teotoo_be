ff012db7623417b40ca3c4ef9c6010ae8fa8924d
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2024-04-03 22:08:13 +0900

    TrainerInfoServiceTest 테스트코드 수정

--- src/test/java/com/project/durumoongsil/teutoo/trainer/info/service/TrainerInfoServiceTest.java
+++ src/test/java/com/project/durumoongsil/teutoo/trainer/info/service/TrainerInfoServiceTest.java
@@ -25,6 +25,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -226,13 +227,18 @@ class TrainerInfoServiceTest {
             testTrainerInfoList.add(testTrainerInfo);
         }
 
-        TrainerListReqDto trainerListReqDtoMock = mock(TrainerListReqDto.class);
+        TrainerListReqDto testTrainerListReqDto = new TrainerListReqDto();
+        testTrainerListReqDto.setDirection("asc");
+        testTrainerListReqDto.setPage(10);
+        testTrainerListReqDto.setSize(1);
+        testTrainerListReqDto.setSort("alpha");
+        testTrainerListReqDto.setSearch("aaa");
 
         Page<TrainerInfo> mockTrainerInfoPage = new PageImpl<>(testTrainerInfoList);
 
-        when(trainerInfoRepository.findBySearchCondition(trainerListReqDtoMock)).thenReturn(mockTrainerInfoPage);
+        when(trainerInfoRepository.findBySearchCondition(any(PageRequest.class), eq("aaa"))).thenReturn(mockTrainerInfoPage);
 
-        List<TrainerSummaryResDto> trainerSummaryResDtoList = trainerInfoService.getTrainerList(trainerListReqDtoMock).getContent();
+        List<TrainerSummaryResDto> trainerSummaryResDtoList = trainerInfoService.getTrainerList(testTrainerListReqDto).getContent();
 
         // return 된 값 크기 검사
         assertEquals(trainerSummaryResDtoList.size(), 5);
